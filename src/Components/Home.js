f1a299d4a75a7fb63ab0432311bcaa75c75e2247
Kemal Yukselir <k.yukselir123@gmail.com>
2024-04-15 16:54:53 +0100

    inital push

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from './NavigationBar'
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <Navigation />
+    </div>
+  )
+}
+
+export default Home

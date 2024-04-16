5a26baeab0983e95935412495fd39d21a0048842
Kemal Yukselir <k.yukselir123@gmail.com>
2024-04-16 22:42:51 +0100

    added final year project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Components/Home';
 import Portfolios from './Components/Portfolios';
 import MafiaGameImg from "./Assets/mafiaGameImg.png";
+import groupProjectImg from "./Assets/groupDissImg.png";
 
 
 function App() {
@@ -14,6 +15,17 @@ function App() {
         githubLink="[Private]"
         deploymentLink="https://themafiagame.netlify.app/"
         imgPlaceHolder= {MafiaGameImg}
+        imgWidth = "550px"
+        imgHeight = "600px"
+      />
+      <Portfolios
+        projectName="CyberTrace - Digital Forensics Game (Final Year Project)"
+        gameStack="HTML/CSS/JS, Java, Springboot, MySQL"
+        githubLink="[Private]"
+        deploymentLink = {null}
+        imgPlaceHolder= {groupProjectImg}
+        imgWidth = "1300px"
+        imgHeight = "450px"
       />
     </div>
     
--- src/Components/Portfolios.js
+++ src/Components/Portfolios.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
-const Portfolios = ({ projectName, gameStack, githubLink, deploymentLink, imgPlaceHolder }) => {
+const Portfolios = ({ projectName, gameStack, githubLink, deploymentLink, imgPlaceHolder, imgWidth, imgHeight }) => {
   return (
     <div>
         <div className='portfolio-section'>
           <div className='img-wrapper'>
-            <img src={imgPlaceHolder} alt="Placeholder"/>
+            <img src={imgPlaceHolder} alt="Placeholder" style={{ width: imgWidth, height: imgHeight }}/>
           </div>
           <div className='text-wrapper'>
               <h1>Project Name:</h1> <p>{projectName}</p>
               <h1>Game Stack:</h1> <p>{gameStack}</p>
               <h1>GitHub:</h1> <p>{githubLink}</p>
-              <h1>Deployment:</h1> <a href={deploymentLink} target="_blank" rel="noreferrer">{deploymentLink}</a>
+              {deploymentLink != null ? (
+              <>
+                <h1>Deployment:</h1><a href={deploymentLink} target="_blank" rel="noreferrer">{deploymentLink}</a>
+              </>
+            ) : (
+              <>
+                <h1>Deployment:</h1> <p>No Deployment</p>
+              </>)}
           </div>
         </div>
     </div>

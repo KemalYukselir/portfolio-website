0e7f492f0e5c598888d0ce92fe8a931e3354d725
Kemal Yukselir <K.Yukselir123@gmail.com>
2024-04-24 13:16:43 +0100

    Changed homescreen to have a terminal like feeling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Navigation from './NavigationBar'
 import { FaReact } from "react-icons/fa";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { AiOutlinePlus } from "react-icons/ai";
+
 
 
 const Home = () => {
@@ -8,19 +11,33 @@ const Home = () => {
     <div className='home-container'>
       <Navigation />
       <div className="home-section">
+          <div className='home-heading-section'>
           <h1 id="primary-heading">
-            Hello, I'm Kemal Yukselir...|
+            C:\Users\Kemal Yukselir{">"}<span className="vertical-line-animation"> |</span>
           </h1>
-          <p id="primary-text">
-            Welcome to my portfolio, which consists of projects I have completed or I'm working on. <br/>
-            Check out my contact information section to access my LinkedIn, etc.
-          </p>
-          <p id="primary-text">
-            Feel free to scroll through the project images with the buttons above it. <br/>
-          </p>
-          <p id="primary-text">
-            This website was made with react.{<FaReact/>}<a href="https://github.com/KemalYukselir/portfolio-website" target="_blank" rel="noreferrer">[Github link]</a> 
-          </p>
+          </div>
+
+          <div class='terminal-screen'> 
+            <a class='terminal-selections'>
+            <span class='hidden-text'>PROBLEMS OUTPUT DEBUG CONSOLE <span style={{ color: 'green',  textDecoration: 'underline'}}>TERMINAL</span> PORTS</span>
+              <span class='terminal-option'><AiOutlinePlus/> <FaRegTrashAlt/> ...</span>
+            </a>
+
+            <br/>
+            <br/>
+            <div className='terminal-text'>
+              <a class='terminal-info'>[ <span>Info</span> ] :</a> <a>Welcome to my portfolio, which consists of projects I have completed or I'm working on. 
+                Check out my contact information section to access my LinkedIn, etc.</a>
+              <br/>
+              <br/>
+              <a class='terminal-info'>[ <span>Info</span> ] :</a> <a> Feel free to scroll through the project images with the buttons above it.</a>
+              <br/>
+              <br/>
+              <a class='terminal-info'>[ <span>Info</span> ] :</a> <a> This website was made with react.{<FaReact/>}<a href="https://github.com/KemalYukselir/portfolio-website" target="_blank" rel="noreferrer">[Github link]</a></a>
+              <br/>
+              <span className="vertical-line-animation">▯</span>
+            </div>
+          </div>
       </div>
     </div>
   )
--- src/Components/Portfolios.js
+++ src/Components/Portfolios.js
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
-import { HiOutlineArrowRight} from "react-icons/hi";
+import React, { useState, useEffect } from 'react';
+import { HiOutlineArrowRight } from "react-icons/hi";
 
-
-
-const Portfolios = ({ projectName, gameStack, projectInformation, githubLink, deploymentLink, imgPlaceHolder, imgWidth, imgHeight, flip }) => {
-  // Check if the viewport width is less than or equal to 600px
-const mediaQuery = window.matchMedia("(max-width: 600px)");
-
-// Define a callback function to execute when the media query matches
-const handleMediaQueryChange = (mediaQueryList) => {
-  if (mediaQueryList.matches) {
-    flip = true;
-  } 
-  };
-
-  // Initially, call the callback function to check if the media query matches
-  handleMediaQueryChange(mediaQuery);
-  
-  
+const Portfolios = ({
+  projectName,
+  gameStack,
+  projectInformation,
+  githubLink,
+  deploymentLink,
+  imgPlaceHolder,
+  imgWidth,
+  imgHeight,
+}) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [flip, setFlip] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 600px)");
+
+    const handleMediaQueryChange = (mediaQueryList) => {
+      setFlip(mediaQueryList.matches);
+    };
+
+    handleMediaQueryChange(mediaQuery);
+
+    mediaQuery.addListener(handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
 
   const nextImage = () => {
     setCurrentImageIndex(prevIndex => (prevIndex + 1) % imgPlaceHolder.length);
@@ -29,86 +39,36 @@ const handleMediaQueryChange = (mediaQueryList) => {
   };
 
   return (
-    <div>
-      { flip ? 
-        (
-          <div className='portfolio-section'>
-            <div className='img-container'>
-              <div className="button-container">
-                <button className='previous-image' onClick={previousImage}>&lt;</button>
-                <button className='next-image' onClick={nextImage}>&gt;</button>
-              </div>
-              <div className='img-wrapper'>
-                {/* Image */}
-                <img src={imgPlaceHolder[currentImageIndex]} alt="Placeholder" style={{ width: imgWidth, height: imgHeight }}/>
-              </div>
-            </div>
-
-            <div className='text-wrapper'>
-              {/* Project Details */}
-              <h1>Project Name And Tech Stack:</h1> <p>{projectName} <br/> {gameStack}</p>
-              <h1>Project Information:</h1><p>{projectInformation}</p>
-              {githubLink != null ? (
-                <>
-                  <h1>GitHub:</h1><a href={githubLink} target="_blank" rel="noreferrer">Click me
-                  <HiOutlineArrowRight style={{marginLeft: '5%' }}/></a>
-                </>
-              ) : (
-                <>
-                  <h1>GitHub:</h1> <p>[Private]</p>
-                </>)}
-
-              {deploymentLink != null ? (
-                <>
-                  <h1>Deployment or Video:</h1><a href={deploymentLink} target="_blank" rel="noreferrer">Click me
-                  <HiOutlineArrowRight style={{marginLeft: '5%' }}/></a>
-                </>
-              ) : (
-                <>
-                  <h1>Deployment or Video:</h1> <p>[Private]</p>
-                </>)}
-            </div>
-          </div>
+    <div className='portfolio-section'>
+      <div className={flip ? 'img-container-flip' : 'img-container'}>
+        <div className="button-container">
+          <button className='previous-image' onClick={previousImage}>&lt;</button>
+          <button className='next-image' onClick={nextImage}>&gt;</button>
+        </div>
+        <div className='img-wrapper'>
+          <img src={imgPlaceHolder[currentImageIndex]} alt="Placeholder" style={{ width: imgWidth, height: imgHeight }} />
+        </div>
+      </div>
+      <div className='text-wrapper'>
+        <h1>Project Name And Tech Stack:</h1>
+        <p>{projectName} <br /> {gameStack}</p>
+        <h1>Project Information:</h1>
+        <p>{projectInformation}</p>
+        <h1>GitHub:</h1>
+        {githubLink != null ? (
+          <a href={githubLink} target="_blank" rel="noreferrer">Click me <HiOutlineArrowRight style={{ marginLeft: '5%' }} /></a>
         ) : (
-          <div className='portfolio-section'>
-            <div className='text-wrapper'>
-              {/* Project Details */}
-              <h1>Project Name And Tech Stack:</h1> <p>{projectName} <br/> {gameStack}</p>
-              <h1>Project Information:</h1><p>{projectInformation}</p>
-              {githubLink != null ? (
-                <>
-                  <h1>GitHub:</h1><a href={githubLink} target="_blank" rel="noreferrer">Click me
-                  <HiOutlineArrowRight style={{marginLeft: '5%' }}/></a>
-                </>
-              ) : (
-                <>
-                  <h1>GitHub:</h1> <p>[Private]</p>
-                </>)}              
-                {deploymentLink != null ? (
-                <>
-                  <h1>Deployment or Video:</h1><a href={deploymentLink} target="_blank" rel="noreferrer">Click me
-                  <HiOutlineArrowRight style={{marginLeft: '5%' }}/></a>
-                </>
-              ) : (
-                <>
-                  <h1>Deployment or Video:</h1> <p>[Private]</p>
-                </>)}
-            </div>
-            <div className='img-container'>
-              <div className="button-container">
-                <button className='previous-image' onClick={previousImage}>&lt;</button>
-                <button className='next-image' onClick={nextImage}>&gt;</button>
-              </div>
-              <div className='img-wrapper'>
-                {/* Image */}
-                <img src={imgPlaceHolder[currentImageIndex]} alt="Placeholder" style={{ width: imgWidth, height: imgHeight }}/>
-              </div>
-            </div>
-          </div>
-        )
-      }
+          <p>[Private]</p>
+        )}
+        <h1>Deployment or Video:</h1>
+        {deploymentLink != null ? (
+          <a href={deploymentLink} target="_blank" rel="noreferrer">Click me <HiOutlineArrowRight style={{ marginLeft: '5%' }} /></a>
+        ) : (
+          <p>[Private]</p>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Portfolios;

f54d3be3bd1b36b71abc926592cee093d0d58c7f
Kemal Yukselir <k.yukselir123@gmail.com>
2025-01-11 16:59:31 +0000

    added about me to nav bar

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -3,6 +3,7 @@ import { HiOutlineBars3 } from "react-icons/hi2";
 import { RiContactsBook3Fill } from "react-icons/ri";
 import { MdOutlineDesignServices } from "react-icons/md";
 import { FaCode } from "react-icons/fa";
+import { FaCircleInfo } from "react-icons/fa6";
 
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
@@ -23,14 +24,17 @@ const NavigationBar = () => {
   const handleHomeClick = () => {
     window.scrollTo(0, document.body.scrollHeight / 100);
   };
+  const handleAboutMeClick = () => {
+    window.scrollTo(0, document.body.scrollHeight / 7.5);
+  };
   const handleContactClick = () => {
-    window.scrollTo(0, document.body.scrollHeight);
+    window.scrollTo(0, document.body.scrollHeight / 1.3);
   };
   const handlePortfoliosClick = () => {
-    window.scrollTo(0, document.body.scrollHeight / 2.1);
+    window.scrollTo(0, document.body.scrollHeight / 2);
   };
   const handleSkillsClick = () => {
-    window.scrollTo(0, document.body.scrollHeight / 7.5);
+    window.scrollTo(0, document.body.scrollHeight / 3);
   };
 
   const navBarOptions = [
@@ -38,6 +42,11 @@ const NavigationBar = () => {
       text: "Home",
       icon: <HomeIcon/>,
       onClick: handleHomeClick
+    },
+    {
+      text: "About me",
+      icon: <FaCircleInfo/>,
+      onClick: handleAboutMeClick
     },
     {
       text: "Skills",
@@ -61,6 +70,7 @@ const NavigationBar = () => {
     <nav>
       <div className="navbar-links-container">
         <a href="#home" onClick={handleHomeClick}> Home</a>
+        <a href="#about me" onClick={handleAboutMeClick}> <FaCircleInfo className="navBar-icon"/> About me</a>
         <a href="#Skills" onClick={handleSkillsClick}> <FaCode className="navBar-icon"/> Skills</a>
         <a href="#portfolios" onClick={handlePortfoliosClick}> <MdOutlineDesignServices className="navBar-icon"/> Projects</a>
         <a href="#contact" onClick={handleContactClick}> <RiContactsBook3Fill className="navBar-icon"/> Contact</a>

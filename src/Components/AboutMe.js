dac7b2fe580b58208b167b047456fe79d61e8c31
Kemal Yukselir <k.yukselir123@gmail.com>
2025-01-12 19:24:53 +0000

    Removed past experience in about me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -4,9 +4,8 @@ const AboutMe = () => {
   return (
     <div className="aboutme-section-wrapper">
       <div className="aboutme-section-top">
-        <h1>- About me! -</h1>
+        <h1>- About me -</h1>
         <p>Hi, I’m Kemal Yukselir, a dedicated and adaptable professional with a First Class Honours degree in Computer Science from the University of Kent. My journey into technology began in 2020, inspired by a passion for solving problems and creating impactful solutions. Since then, I’ve worked on a wide range of projects, leveraging various tools, frameworks, and technologies to deliver high-quality results.
-<br/><br/> In my recent role at Caseware UK, I collaborated on improving the Cloud Financial Reporting Statements project, enhancing product quality, and addressing complex user challenges. My portfolio features diverse work, including responsive websites, full stack applications, and automation tools, showcasing my ability to design and implement efficient solutions tailored to users’ needs.
 <br/><br/>I’m passionate about innovation, particularly the integration of AI and machine learning into modern development, and I thrive in collaborative environments that encourage creativity and growth. With experience in cloud platforms and modern development practices, I am committed to delivering solutions that make a difference.
 <br/><br/>Outside of work, I enjoy staying active through boxing and football, as well as exploring creative and technical pursuits.</p>
       </div>
